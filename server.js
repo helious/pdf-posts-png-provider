d2915a17fa795409b89b8514c76506c0a24b445f
Posey <rangemonk@gmail.com>
2015-06-25 01:44:41 -0400

    works!

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 function startExpress() {
   app.use(function (req, res, next) {
     if (req.method === 'GET' && /^\/[\d\w]+(?=$|[\/?#])/.test(req.url)) {
-      req.url = req.url.replace(/^\/([\d\w]+).*$/, "/$1.html");
+      req.url = req.url.replace(/^\/([\d\w]+).*$/, '/$1.html');
     }
 
     if (req.url === '/') {
@@ -14,8 +14,14 @@ function startExpress() {
   app.use(multer({ dest: './tmp/' }));
   app.use(multerAutoReap);
 
+  app.post('/deploy', function getDeploy(req, res) {
+    fs.rename('public/staging.png', 'public/pdf.png', function afterRename() {
+      res.redirect('/success');
+    });
+  });
+
   app.post('/upload', function postUpload(req, res) {
-    exec('convert -density 400 -trim ' + req.files['userPDF'].path + ' -quality 100 public/staging.png', function afterConvert(error, stdout, stderr) {
+    exec('convert -density 400 ' + req.files['userPDF'].path + ' -quality 100 public/staging.png', function afterConvert(error, stdout, stderr) {
       res.redirect('/confirm');
     });
   });
@@ -25,7 +31,8 @@ function startExpress() {
   app.listen(port, function() {});
 }
 
-var exec            = require('child_process').exec,
+var fs              = require('fs'),
+    exec            = require('child_process').exec,
     express         = require('express'),
     app             = express(),
     multer          = require('multer'),
