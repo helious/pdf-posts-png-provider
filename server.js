711513cd2b947df8f4b207d1f48120973e94f8d9
Posey <rangemonk@gmail.com>
2015-06-25 01:33:29 -0400

    this should work

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-function setupRoutes() {
+function startExpress() {
   app.use(function (req, res, next) {
     if (req.method === 'GET' && /^\/[\d\w]+(?=$|[\/?#])/.test(req.url)) {
       req.url = req.url.replace(/^\/([\d\w]+).*$/, "/$1.html");
@@ -15,28 +15,21 @@ function setupRoutes() {
   app.use(multerAutoReap);
 
   app.post('/upload', function postUpload(req, res) {
-    var pdfImage = new PDFImage(req.files['userPDF'].path);
-
-    pdfImage.convertPage(0).then(function onPageConversion(imagePath) {
-      fs.rename(imagePath, 'public/staging.png', function onImageRename(err) {
-        res.redirect('/confirm'); 
-      });
+    exec('convert -density 400 -trim ' + req.files['userPDF'].path + ' -quality 100 public/staging.png', function afterConvert(error, stdout, stderr) {
+      res.redirect('/confirm');
     });
   });
 
   app.use(express.static(__dirname + '/public'));
+
+  app.listen(port, function() {});
 }
 
-var fs              = require('fs'),
+var exec            = require('child_process').exec,
     express         = require('express'),
     app             = express(),
     multer          = require('multer'),
     multerAutoReap  = require('multer-autoreap'),
-    PDFImage        = require("pdf-image").PDFImage,
     port            = process.env.PORT || 5000;
 
-setupRoutes();
-
-app.listen(port, function() {
-  console.log('Node app is running on port', port);
-});
+startExpress();
