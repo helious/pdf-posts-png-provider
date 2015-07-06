c1bff214289c987fdd266550f5981120e1ba1a31
Posey <rangemonk@gmail.com>
2015-07-05 23:14:51 -0400

    auth.

--- server.js
+++ server.js
@@ -1,18 +1,34 @@
 function startExpress() {
   app.use(function (req, res, next) {
     if (req.method === 'GET' && /^\/[\d\w]+(?=$|[\/?#])/.test(req.url)) {
+      function authenticate() {
+        var credentials = basicAuth(req);
+
+        if (!credentials || credentials.name !== process.env.CDN_USER || credentials.pass !== process.env.CDN_PASS) {
+          res.setHeader('WWW-Authenticate', 'Basic');
+          res.statusCode = 401;
+          res.end();
+        }
+      }
+
       if (req.url.indexOf('/images') < 0 && req.url.indexOf('/fonts') < 0) {
+        authenticate();
+
         req.url = req.url.replace(/^\/([\d\w]+).*$/, '/$1.html');
       } else {
-        res.header("Access-Control-Allow-Origin", "*");
+        res.setHeader('Access-Control-Allow-Origin', '*');
       }
     }
 
     if (req.url === '/') {
+      authenticate();
+
       req.url = '/index.html';
     }
 
-    next();
+    if (res.statusCode !== 401) {
+      next();
+    }
   });
 
   app.use(multer({ dest: './tmp/' }));
@@ -41,6 +57,7 @@ var fs              = require('fs'),
     app             = express(),
     multer          = require('multer'),
     multerAutoReap  = require('multer-autoreap'),
+    basicAuth       = require('basic-auth'),
     port            = process.env.PORT || 5000;
 
 startExpress();
